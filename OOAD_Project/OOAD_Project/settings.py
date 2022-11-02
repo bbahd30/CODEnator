afd100311d253f63a05fee5b3511feff5ef02750
bbahd30 <b_bonal@cs.iitr.ac.in>
2022-11-02 09:52:03 +0530

    Add corsheaders

--- OOAD_Project/OOAD_Project/settings.py
+++ OOAD_Project/OOAD_Project/settings.py
@@ -16,20 +16,32 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = (
+    'http://localhost:3000',
+    'http://10.26.2.242:3000'
+)
 
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000'
+)
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CODEnator'
+    'CODEnator',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
