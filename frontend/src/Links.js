15e5ddc3311760d6dd50132aadb1a7cd5670ccee
bbahd30 <b_bonal@cs.iitr.ac.in>
2022-11-02 19:16:42 +0530

    Add Links

--- frontend/src/Links.js
+++ frontend/src/Links.js
@@ -1,2 +1,14 @@
-export const localhost = 'http://127.0.0.1:8000/'
-export const paragraphs_api = `${localhost}paragraphs/`
+export const localhost = 'http://127.0.0.1:8000/';
+
+export const get_paragraphs_api = `${localhost}paragraphslatest/`;
+export const get_tags_api = `${localhost}tagslatest/`;
+export const get_image_api = `${localhost}imageslatest/`;
+export const get_anchors_api = `${localhost}anchorslatest/`;
+
+
+export const post_tags_api = `${localhost}tags/`;
+export const post_image_api = `${localhost}images/`;
+export const image_upload = `${localhost}upload/`;
+export const post_paragraphs_api = `${localhost}paragraphs/`;
+export const post_anchors_api = `${localhost}anchors/`;
+
