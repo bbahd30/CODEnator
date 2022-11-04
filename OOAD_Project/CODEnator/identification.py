9d54eda346639b81add16db91990d941766a421d
bbahd30 <b_bonal@cs.iitr.ac.in>
2022-11-04 22:00:20 +0530

    Add links and tags for components

---
+++ OOAD_Project/CODEnator/identification.py
@@ -0,0 +1,82 @@
+# from ast import main
+from main_prog import my_dict
+
+components = []
+f_prg = open("program.html", "w")
+f_prg.write("<HTML>\n")
+f_prg.write("<BODY>\n")
+for i in reversed(my_dict) :
+    print(i)
+    if i == (254,0,0) :
+        f_prg.write("\t<h1>This is Heading</h1>\n")
+        components.append('h1')
+    elif i == (255,139,0) :
+        f_prg.write("\t<h2>This is Heading</h2>\n")
+        components.append('h2')
+    elif i == (154,205,50) :
+        f_prg.write("\t<h3>This is Heading</h3>\n")
+        components.append('h3')
+    elif i == (32,178,170) :
+        f_prg.write("\t<h4>This is Heading</h4>\n")
+        components.append('h4')
+    elif i == (100,149,237) :
+        f_prg.write("\t<h5>This is Heading</h5>\n")
+        components.append('h5')
+    elif i == (219,112,147) :
+        f_prg.write("\t<h6>This is Heading</h6>\n")
+        components.append('h6')
+    elif i == (255,0,254) :
+        f_prg.write("\t<p>lorem*15</p>\n")
+        components.append('paragraph')
+    elif i == (0,255,0) :
+        f_prg.write("\t<hr>")
+        components.append('hr')
+    elif i == (0,0,128) :
+        f_prg.write("\t<a>Thisishyperlink.com</a>\n")
+        components.append('anchor')
+    elif i == (255,215,0) :
+        f_prg.write("   <img src=\"image.jpg\" alt=\"Your face\" width=\"42\" height=\"42\">\n")
+        components.append('anchor')
+    elif i == (250,128,114) :
+        f_prg.write("\t<ul>\n"
+                            "\t\t<li style = \"float:left\"><a href=\"#home\">Home</a></li>\n"
+                            "\t\t<li style = \"float:left\"><a href=\"#news\">News</a></li>\n"
+                            "\t\t<li style = \"float:left\"><a href=\"#contact\">Contact</a></li>\n"
+                            "\t\t<li style = \"float:left\"><a href=\"#about\">About</a></li>\n"
+                        "\t</ul>")
+        components.append('navbar')
+    elif i == (127,255,212) :
+        f_prg.write("\t<table style=\"width:100%\">\n"
+                        "\t\t<tr>\n"
+                        "\t\t<th style=\"border:1px solid black\">Person 1</th>\n"
+                        "\t\t<th style=\"border:1px solid black\">Person 2</th>\n"
+                        "\t\t<th style=\"border:1px solid black\">Person 2</th>\n"
+                        "\t\t</tr>\n"
+                        "\t\t<tr>\n"
+                        "\t\t<td style=\"border:1px solid black\">Billy</td>\n"
+                        "\t\t<td style=\"border:1px solid black\">Gilly</td>\n"
+                        "\t\t<td style=\"border:1px solid black\">Silly</td>\n"
+                        "\t\t</tr>\n"
+                        "\t\t<tr>\n"
+                        "\t\t<td style=\"border:1px solid black\">16</td>\n"
+                        "\t\t<td style=\"border:1px solid black\">14</td>\n"
+                        "\t\t<td style=\"border:1px solid black\">12</td>\n"
+                        "\t\t</tr>\n"
+                        "\t\t</table>)\n")
+        components.append('table')
+    elif i == (127,255,0) :
+        f_prg.write("\t<button type=\"button\">Click Me!</button>\n")
+        components.append('button')
+    elif i == (88,88,88) :
+        f_prg.write("\t<label for=\"cars\">Choose a car:</label>\n"
+                    "\t\t<select>\n"
+                    "\t\t<option value=\"option1\">Option A</option>\n"
+                    "\t\t<option value=\"option2\">Option B</option>\n"
+                    "\t\t<option value=\"option3\">Option C</option>\n"
+                    "\t\t<option value=\"option4\">Option D</option>\n"
+                    "\t\t</select>\n")
+        components.append('select')
+print(components) 
+f_prg.write("</BODY>\n")
+f_prg.write("</HTML>\n")
+f_prg.close()
--- OOAD_Project/OOAD_Project/urls.py
+++ OOAD_Project/OOAD_Project/urls.py
@@ -9,7 +9,13 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 router.register(r'images', views.ImageViewset, basename='images'),
-router.register(r'headings', views.HeadingViewset, basename='headings'),
+# router.register(r'headings', views.HeadingViewset, basename='headings'),
+router.register(r'h1', views.H1Viewset, basename='h1'), #changed
+router.register(r'h2', views.H2Viewset, basename='h2'), #changed
+router.register(r'h3', views.H3Viewset, basename='h3'), #changed
+router.register(r'h4', views.H4Viewset, basename='h4'), #changed 
+router.register(r'h5', views.H5Viewset, basename='h5'), #changed
+router.register(r'h6', views.H6Viewset, basename='h6'), #changed
 router.register(r'paragraphs', views.ParagraphViewset, basename='paragraphs'),
 router.register(r'hrs', views.HrViewset, basename='hrs'),
 router.register(r'anchors', views.AnchorViewset, basename='anchors'),
@@ -18,7 +24,7 @@ router.register(r'navbars', views.NavbarViewset, basename='navbars'),
 router.register(r'tables', views.TableViewset, basename='tables'),
 router.register(r'buttons', views.ButtonViewset, basename='buttons'),
 router.register(r'selects', views.SelectViewset, basename='selects'),
-router.register(r'sidebars', views.SidebarViewset, basename='sidebars'),
+# router.register(r'sidebars', views.SidebarViewset, basename='sidebars'),
 router.register(r'tags', views.TagViewset, basename='tags'),
 
 
@@ -38,14 +44,54 @@ urlpatterns = [
     ({
         'get': 'get_data'
     }), name='tags'),
+    path('h1latest/', views.H1Viewset.as_view
+    ({
+        'get': 'get_data'
+    }), name='h1'),
+    path('h2latest/', views.H2Viewset.as_view
+    ({
+        'get': 'get_data'
+    }), name='h2'),
+    path('h3latest/', views.H3Viewset.as_view
+    ({
+        'get': 'get_data'
+    }), name='h3'),
+    path('h4latest/', views.H4Viewset.as_view
+    ({
+        'get': 'get_data'
+    }), name='h4'),
+    path('h5latest/', views.H5Viewset.as_view
+    ({
+        'get': 'get_data'
+    }), name='h5'),
+    path('h6latest/', views.H6Viewset.as_view
+    ({
+        'get': 'get_data'
+    }), name='h6'),
     path('paragraphslatest/', views.ParagraphViewset.as_view
     ({
         'get': 'get_data'
     }), name='paragraphs'),
-     path('anchorslatest/', views.AnchorViewset.as_view
+    path('hrslatest/', views.HrViewset.as_view
+    ({
+        'get': 'get_data'
+    }), name='hrs'),
+    path('anchorslatest/', views.AnchorViewset.as_view
     ({
         'get': 'get_data'
     }), name='anchors'),
+    path('navbarslatest/', views.NavbarViewset.as_view
+    ({
+        'get': 'get_data'
+    }), name='navbars'),
+    path('tableslatest/', views.TableViewset.as_view
+    ({
+        'get': 'get_data'
+    }), name='tables'),
+    path('selectslatest/', views.SelectViewset.as_view
+    ({
+        'get': 'get_data'
+    }), name='selects'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
